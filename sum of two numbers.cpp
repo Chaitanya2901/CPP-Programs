5f97c9b3be0acdd38598df20bf79dbada0601b9c
Chaitanya2901 <102059139+Chaitanya2901@users.noreply.github.com>
2022-03-21 22:47:06 +0530

    Add files via upload

---
+++ sum of two numbers.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    int sum;
+    cout<<"type the numbers"<<endl;
+    cin >> a;
+    cin >> b;
+    sum = a+b;
+    cout << "this is sum" << sum;
+}
